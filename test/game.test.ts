bc470fa6801f383cc9284ff33f6f8a119e8f7478
JT Smith <jt@plainblack.com>
2024-03-22 13:46:15 -0500

    deeper testing

--- test/game.test.ts
+++ test/game.test.ts
@@ -1,6 +1,7 @@
 import { expect, test, beforeEach } from 'vitest'
 import { TestRunner } from "@boardzilla/core";
 import { MyGame, SevenPlayer, Card, default as setup } from '../src/game/index.js';
+import { scorePlayer } from '../src/game/scoring.js';
 
 //@ts-ignore
 let runner: TestRunner<SevenPlayer, MyGame>;
@@ -15,53 +16,84 @@ test('allows you to take turns', () => {
     settings: {}
   });
 
-
-  console.log('player 1 stuff', p1);
-  console.log('player 2 stuff', p1);
-
   expect(runner.server.game.round).toBe(7);
 
+  expect(p1.player.my('hand')?.all(Card).length).toBe(11);
+  expect(p2.player.my('hand')?.all(Card).length).toBe(11);
 
-  expect(p1.actions()).toStrictEqual(['discardCard'])
-  expect(p2.actions()).toStrictEqual(['discardCard'])
+  expect(p1.actions()).toStrictEqual(['discardCard']);
+  expect(p2.actions()).toStrictEqual(['discardCard']);
 
-  console.log('p1 hand', p1.player.my('hand')?.all(Card).map(card => card.name))
+  // console.log('p1 hand', p1.player.my('hand')?.all(Card).map(card => card.name))
   p1.move('discardCard', {
     //@ts-ignore
     card: p1.player.my('hand')!.last(Card)!
   });
 
-  expect(p1.actions()).toStrictEqual([])
-  expect(p2.actions()).toStrictEqual(['discardCard'])
+  expect(p1.player.my('hand')?.all(Card).length).toBe(10);
 
-  console.log('p2 hand', p1.player.my('hand')?.all(Card).map(card => card.name))
+  expect(p1.actions()).toStrictEqual([]);
+  expect(p2.actions()).toStrictEqual(['discardCard']);
+
+  //  console.log('p2 hand', p2.player.my('hand')?.all(Card).map(card => card.name))
   p2.move('discardCard', {
     //@ts-ignore
-    card: p1.player.my('hand')!.last(Card)!
+    card: p2.player.my('hand')!.last(Card)!
   });
 
+  expect(p2.player.my('hand')?.all(Card).length).toBe(10);
 
-  //expect(p1.actions()).toStrictEqual(['discardDown'])
-  // expect(p2.actions()).toStrictEqual(['discardDown'])
+  expect(p1.actions()).toStrictEqual(['discardDown']);
+  expect(p2.actions()).toStrictEqual(['discardDown']);
+
+  p1.move('discardDown', {
+    //@ts-ignore
+    cards: p1.player.my('hand')!.firstN(3, Card)!
+  });
+
+  expect(p1.player.my('hand')?.all(Card).length).toBe(7);
+
+  expect(p1.actions()).toStrictEqual([]);
+  expect(p2.actions()).toStrictEqual(['discardDown']);
+
+  p2.move('discardDown', {
+    //@ts-ignore
+    cards: p2.player.my('hand')!.firstN(3, Card)!
+  });
+
+  expect(p2.player.my('hand')?.all(Card).length).toBe(11);
+
+  expect(p1.actions()).toStrictEqual(['discardCard'])
+  expect(p2.actions()).toStrictEqual(['discardCard'])
 
 
-})
-/*
-test("doesn't allow one player to play twice", () => {
-  const [ui1, ui2] = runner.start({
+});
+
+test('player hand scoring', () => {
+  const [p1, p2] = runner.start({
     players: 2,
-    settings: { tokens: 4 }
+    settings: {}
   });
 
-  expect(ui1.actions()).toStrictEqual(['take'])
-  expect(ui2.actions()).toStrictEqual([])
 
-  ui1.move('take', {
-    token: ui1.game.first('pool')!.last(Token)!
-  });
+  p1.player.my('hand')?.all().putInto(runner.server.game.first('mess')!)
+  expect(p1.player.my('hand')?.all().length).toBe(0);
+  p2.player.my('hand')?.all().putInto(runner.server.game.first('mess')!)
+  expect(p2.player.my('hand')?.all().length).toBe(0);
 
-  expect(() => ui1.move('take', {
-    token: ui1.game.first('pool')!.last(Token)!
-  })).toThrowError()
-})
-    */
+  //console.log('mess', runner.server.game.all('mess').all(Card).map(card => card.name))
+
+  for (const value of [1, 2, 3, 4, 5, 6, 7])
+    runner.server.game.first('mess')?.firstN(1, Card, { name: 'blue' + value }).putInto(p1.player.my('hand')!);
+
+  //console.log('p1 hand', p1.player.my('hand')?.all(Card).map(card => card.name))
+
+  expect(p1.player.my('hand')?.all().length).toBe(7);
+
+  expect(p1.player.score).toBe(0);
+
+  scorePlayer(p1.player, runner.server.game);
+
+  expect(p1.player.score).toBe(7);
+
+});
